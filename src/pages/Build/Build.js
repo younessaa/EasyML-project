5595e1a091d106bc71936000b6b32bb4fec86372
Youness Aabaoui <y1o2u3n4@gmail.com>
2022-04-10 05:15:09 +0100

    client

--- src/pages/Build/Build.js
+++ src/pages/Build/Build.js
@@ -93,7 +93,7 @@ function Build() {
       for(let i = 0; i < targets.length; i++) {
         data.append('targets', targets[i]);
       }
-      axios.post('//easyml-app.herokuapp.com/buildmode', data ,{
+      axios.post('//easyml-app.herokuapp.com/buildmodel', data ,{
         headers: {
           'Access-Control-Allow-Origin' : '*',
         }
