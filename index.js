ffb6ce68637c05b4ca042ee2f78547bd53d2014a
Youness Aabaoui <y1o2u3n4@gmail.com>
2022-04-10 06:23:24 +0100

    server app

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+      // allowed XHR methods  
+      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+      res.send();
+  });
+});
 // app.use(fileUpload());
 app.use('/experts', expertRoutes);
 app.use('/exploitables', exploitableRoutes);
