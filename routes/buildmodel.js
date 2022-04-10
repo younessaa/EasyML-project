d4e9b589d111a4462e797f033f1d39857d3d6ad9
Youness Aabaoui <y1o2u3n4@gmail.com>
2022-04-10 04:43:22 +0100

    server app

--- routes/buildmodel.js
+++ routes/buildmodel.js
@@ -9,6 +9,7 @@ import multer from 'multer'
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+router.all('*', cors());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -36,7 +37,7 @@ const upload =multer({storage:storage}).fields([
   { name: 'targets', maxCount: 1 },
 ]);
 
-router.use(cors());
+
 
 router.post('/buildmodel', (req, res) => {
   
